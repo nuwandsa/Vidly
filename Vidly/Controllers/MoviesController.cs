110f32d921467f11d25b883044e36d2d0e33c0b5
Nuwan <dsa.nuwan@gmail.com>
2019-01-21 00:00:53 +0530

    test

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shreck" };
+            //return View(movie);
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("ID=" + id);
+        }
+
+    }
+}
